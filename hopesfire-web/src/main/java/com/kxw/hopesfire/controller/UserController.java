073107f726bec7d3a3c254988c2afb2d3d0e1768
kangxiongwei <kangxiongwei@meituan.com>
2020-07-03 13:35:13 +0800

    用户管理接口

--- hopesfire-web/src/main/java/com/kxw/hopesfire/controller/UserController.java
+++ hopesfire-web/src/main/java/com/kxw/hopesfire/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.kxw.hopesfire.controller;
 
 import com.kxw.hopesfire.model.HttpBaseModel;
+import com.kxw.hopesfire.model.PagerModel;
 import com.kxw.hopesfire.model.UserModel;
 import com.kxw.hopesfire.service.IUserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 用户管理接口
@@ -23,15 +21,52 @@ public class UserController {
     @Resource
     private IUserService userService;
 
-    @RequestMapping("/user/list")
-    public HttpBaseModel listUsers() {
-        List<UserModel> users = new ArrayList<>();
-        users.add(new UserModel(1L, "user1", "用户1", 0));
-        users.add(new UserModel(2L, "user2", "用户2", 1));
-        users.add(new UserModel(3L, "user3", "用户3", 0));
-        users.add(new UserModel(4L, "user4", "用户4", 1));
-        users.add(new UserModel(5L, "user5", "用户5", 0));
-        return HttpBaseModel.buildSuccess(users);
+    /**
+     * 保存用户信息
+     *
+     * @param user
+     * @return
+     */
+    @PostMapping("/user/save")
+    public HttpBaseModel saveUser(@RequestBody UserModel user) {
+        this.userService.save(user);
+        return HttpBaseModel.buildSuccess(user.getId());
+    }
+
+    /**
+     * 删除用户信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/user/{id}")
+    public HttpBaseModel deleteUser(@PathVariable Long id) {
+        this.userService.delete(id);
+        return HttpBaseModel.buildSuccess(id);
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/user/{id}")
+    public HttpBaseModel getUser(@PathVariable Long id) {
+        UserModel user = userService.get(id);
+        return HttpBaseModel.buildSuccess(user);
+    }
+
+    /**
+     * 分页查询用户列表
+     *
+     * @param user
+     * @return
+     */
+    @PostMapping("/user/find")
+    public HttpBaseModel listUsers(@RequestBody UserModel user) {
+        PagerModel pager = this.userService.find(user);
+        return HttpBaseModel.buildSuccess(pager);
     }
 
 }
