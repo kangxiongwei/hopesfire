9ccd3fe6bcc8f913bb2b920708237af8664e8df8
kangxiongwei <kangxiongwei@meituan.com>
2020-08-04 17:50:09 +0800

    静态页面加载

--- hopesfire-web/src/main/java/com/kxw/hopesfire/web/config/WebMvcConfiguration.java
+++ hopesfire-web/src/main/java/com/kxw/hopesfire/web/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.kxw.hopesfire.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
@@ -11,5 +12,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  */
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
 
 }
