ae1db06a1e87bc472891588d024e9b323e2ffd76
kangxiongwei <kangxiongwei@kuaishou.com>
2020-12-09 08:57:01 +0800

    拆解一下方法

--- hopesfire-view/src/views/ctl/kb/TaskVersion.vue
+++ hopesfire-view/src/views/ctl/kb/TaskVersion.vue
@@ -6,12 +6,13 @@
 </template>
 
 <script>
-    import {Addon, Graph, Shape} from '@antv/x6'
+    import {Addon, Graph} from '@antv/x6'
     import {StartEvent} from "../../../model/StartEventNode";
     import {FillSlot} from "../../../model/FillSlotNode";
     import {InvokeApi} from "../../../model/InvokeApiNode";
     import {ReplyAnswer} from "../../../model/ReplyAnswerNode";
     import {EndEvent} from "../../../model/EndEvent";
+    import {GraphEdge} from "../../../model/GraphEdge";
 
     export default {
         name: "TaskVersion",
@@ -20,10 +21,123 @@
                 graphData: {
                     nodes: [],
                     edges: []
-                }
+                },
+                graph: null,
+                stencil: null,
+                nodes: []
             }
         },
         methods: {
+            /**
+             * 创建画布
+             */
+            initGraph() {
+                this.graph = new Graph({
+                    container: document.getElementById('graph_layout'),
+                    width: 1024,
+                    height: 740,
+                    scroller: false,
+                    snapline: true,
+                    grid: {
+                        size: 10,      //网格大小 10px
+                        visible: true  //渲染网格背景
+                    },
+                    keyboard: {
+                        enabled: true   //键盘快捷键
+                    },
+                    clipboard: {
+                        enabled: true,  //剪切板
+                    },
+                    selecting: {        //选择器
+                        enabled: true,
+                        showNodeSelectionBox: true,  //选中时边框
+                        anchor: 'center',            //锚点
+                        connectionPoint: 'anchor'    //连接点
+                    },
+                    interacting: true,              //开启可交互
+                    connecting: {
+                        snap: true,                 //自动吸附
+                        dangling: false,            //边的起点和终点只能是节点或连接桩
+                        highlight: true,            //拖动边是高亮显示可用的节点或连接桩
+                        createEdge() {
+                            return new GraphEdge(); //创建边
+                        }
+                    }
+                })
+            },
+            /**
+             * 初始化节点
+             */
+            initNodes() {
+                this.nodes = [];
+                const startEvent = new StartEvent();
+                const fillSlot = new FillSlot();
+                const invokeApi = new InvokeApi();
+                const replyAnswer = new ReplyAnswer();
+                const endEvent = new EndEvent();
+                this.nodes.push(startEvent, fillSlot, invokeApi, replyAnswer, endEvent);
+            },
+            /**
+             * 初始化组件库
+             */
+            initStencil() {
+                let stencilLayout = document.getElementById("stencil_layout");
+                this.stencil = new Addon.Stencil({
+                    title: '组件库',
+                    target: this.graph,
+                    stencilGraphWidth: 200,
+                    stencilGraphHeight: 600,
+                    layoutOptions: {
+                        columns: 1
+                    },
+                    groups: [
+                        {
+                            name: 'taskFlow',
+                            title: '组件库',
+                            collapsable: false
+                        }
+                    ]
+                });
+                //画布拖拽
+                stencilLayout.appendChild(this.stencil.container);
+                this.stencil.load(this.nodes, 'taskFlow')
+            },
+            /**
+             * 绑定事件
+             */
+            initEvents() {
+                const graphLayout = document.getElementById("graph_layout");
+                this.graph.bindKey("meta + c", () => {
+                    const cells = this.graph.getSelectedCells();
+                    if (cells.length) {
+                        this.graph.copy(cells)
+                    }
+                    return false
+                })
+                this.graph.bindKey("meta + v", () => {
+                    if (!this.graph.isClipboardEmpty()) {
+                        const cells = this.graph.paste({offset: 32})
+                        this.graph.cleanSelection()
+                        this.graph.select(cells)
+                    }
+                    return false
+                })
+                this.graph.bindKey('backspace', () => {
+                    const cells = this.graph.getSelectedCells()
+                    if (cells.length) {
+                        this.graph.removeCells(cells)
+                    }
+                    return false
+                })
+                this.graph.on('node:mouseenter', () => {
+                    const ports = graphLayout.querySelectorAll('.x6-port-body');
+                    this.showPorts(ports, true)
+                })
+                this.graph.on('node:mouseleave', () => {
+                    const ports = graphLayout.querySelectorAll('.x6-port-body');
+                    this.showPorts(ports, false)
+                })
+            },
             /**
              * 是否显示连接点
              * @param ports
@@ -36,122 +150,16 @@
             }
         },
         mounted() {
-            const graphLayout = document.getElementById("graph_layout");
-            //创建画布
-            const graph = new Graph({
-                container: document.getElementById('graph_layout'),
-                width: 1024,
-                height: 722,
-                scroller: false,
-                snapline: true,
-                grid: {
-                    size: 10,      //网格大小 10px
-                    visible: true  //渲染网格背景
-                },
-                keyboard: {
-                    enabled: true   //键盘快捷键
-                },
-                clipboard: {
-                    enabled: true,  //剪切板
-                },
-                selecting: {        //选择器
-                    enabled: true,
-                    showNodeSelectionBox: true,  //选中时边框
-                    anchor: 'center',            //锚点
-                    connectionPoint: 'anchor'    //连接点
-                },
-                interacting: true,              //开启可交互
-                connecting: {
-                    snap: true,                 //自动吸附
-                    dangling: false,            //边的起点和终点只能是节点或连接桩
-                    highlight: true,            //拖动边是高亮显示可用的节点或连接桩
-                    createEdge() {
-                        return new Shape.Edge({
-                            attrs: {
-                                line: {
-                                    stroke: '#5F95FF',
-                                    strokeWidth: 1,
-                                    targetMarker: {
-                                        name: 'classic',
-                                        size: 8
-                                    }
-                                }
-                            },
-                            router: {
-                                name: 'manhattan'
-                            },
-                        })
-                    },
-                }
-            })
-            //画布拖拽
-            const stencil = new Addon.Stencil({
-                title: '组件库',
-                target: graph,
-                stencilGraphWidth: 200,
-                stencilGraphHeight: 600,
-                layoutOptions: {
-                    columns: 1
-                },
-                groups: [
-                    {
-                        name: 'taskFlow',
-                        title: '组件库',
-                        collapsable: false
-                    }
-                ]
-            });
-            document.getElementById("stencil_layout").appendChild(stencil.container)
-            //自定义节点
-            const startEvent = new StartEvent();
-            const fillSlot = new FillSlot();
-            const invokeApi = new InvokeApi();
-            const replyAnswer = new ReplyAnswer();
-            const endEvent = new EndEvent();
-            //挂在模板节点
-            stencil.load([
-                startEvent, fillSlot, invokeApi, replyAnswer, endEvent
-            ], 'taskFlow')
-            //绑定快捷键
-            graph.bindKey("meta + c", () => {
-                const cells = graph.getSelectedCells();
-                if (cells.length) {
-                    graph.copy(cells)
-                }
-                return false
-            })
-            graph.bindKey("meta + v", () => {
-                if (!graph.isClipboardEmpty()) {
-                    const cells = graph.paste({offset: 32})
-                    graph.cleanSelection()
-                    graph.select(cells)
-                }
-                return false
-            })
-            graph.bindKey('backspace', () => {
-                const cells = graph.getSelectedCells()
-                if (cells.length) {
-                    graph.removeCells(cells)
-                }
-                return false
-            })
-            graph.on('node:mouseenter', (e) => {
-                const ports = graphLayout.querySelectorAll('.x6-port-body');
-                this.showPorts(ports, true)
-            })
-            graph.on('node:mouseleave', () => {
-                const ports = graphLayout.querySelectorAll('.x6-port-body');
-                this.showPorts(ports, false)
-            })
-            //加载数据
-            graph.fromJSON(this.graphData)
+            this.initGraph();
+            this.initNodes();
+            this.initStencil();
+            this.initEvents();
         }
     }
 </script>
 
 <style scoped>
     #task_layout {
-        border: 1px solid red;
         width: 100%;
         height: 100%;
         display: flex;
@@ -159,8 +167,8 @@
     }
 
     #stencil_layout {
-        width: 200px;
-        height: 722px;
+        width: 112px;
+        height: 740px;
         position: relative;
     }
 
