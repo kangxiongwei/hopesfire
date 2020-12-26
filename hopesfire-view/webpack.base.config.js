f795151fe60b63d622382bb9900403fc21eaf847
kangxiongwei <kangxiongwei@kuaishou.com>
2020-12-26 21:57:04 +0800

    先关闭线上压缩功能插件，有点问题

--- hopesfire-view/webpack.base.config.js
+++ hopesfire-view/webpack.base.config.js
@@ -45,6 +45,9 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                },
                 exclude: /node_modules/
             },
             {
