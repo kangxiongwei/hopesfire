69a4d37b0ba20266bbf5ea08073e8eff58a79dc8
kangxiongwei <kangxiongwei@kuaishou.com>
2021-02-27 10:33:48 +0800

    JSGF

---
+++ hopesfire-web/src/main/java/com/kxw/hopesfire/web/controller/TaskController.java
@@ -0,0 +1,31 @@
+package com.kxw.hopesfire.web.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kxw.hopesfire.web.model.HttpBaseModel;
+
+/**
+ * @author kangxiongwei <kangxiongwei@kuaishou.com>
+ * Created on 2021-01-14
+ */
+@RestController
+@RequestMapping("/ctl/task")
+public class TaskController {
+
+
+    /**
+     * 保存task版本信息
+     *
+     * @return
+     */
+    @PostMapping("/version/save")
+    public HttpBaseModel save() {
+
+
+        return HttpBaseModel.buildSuccess(null);
+    }
+
+
+}
